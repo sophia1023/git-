368f3f31d20cf48f6b2ed605aefd4790e5d6eb38
user <user@user-pc>
2017-01-04 10:37:55 +0900

    두번째 테스트

--- Java-GitTest/src/edu/java/git/GitTest.java
+++ Java-GitTest/src/edu/java/git/GitTest.java
@@ -3,7 +3,8 @@ package edu.java.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("GitHub Test");
+		System.out.println("GitHub Testㅇㅇ");
+		System.out.println("두번째 테스트");
 
 	}
 
